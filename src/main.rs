460e00a408a75458a720640b93ad81c82feed757
Sergio García Prado <sergio@garciparedes.me>
2021-02-25 21:51:07 +0100

    * Try reverse strategy.

--- src/main.rs
+++ src/main.rs
@@ -108,7 +108,7 @@ impl Solver {
             let mut cycle = 1;
             let mut last = streets[0].visits;
             for street in streets {
-                if last < street.visits {
+                if last > street.visits {
                     last = street.visits;
                     cycle += 1;
                 }
