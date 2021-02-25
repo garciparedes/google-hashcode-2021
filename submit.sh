16f2dc35f9d35b76c9b0d3007e4e53d6ef72eec7
Sergio García Prado <sergio@garciparedes.me>
2021-02-25 20:22:27 +0100

    * Improve gitignore. * Add missing input file. * Add submit script.

---
+++ submit.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+git commit -a
+
+set -e
+git archive --format zip --output solution.zip HEAD
+
+cargo run < res/a.txt > a.out
+cargo run < res/b.txt > a.out
+cargo run < res/c.txt > c.out
+cargo run < res/d.txt > d.out
+cargo run < res/e.txt > e.out
+cargo run < res/f.txt > f.out
