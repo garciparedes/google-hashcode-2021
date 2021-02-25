cce177951c8a4549fb228857703309791bea83c6
Sergio García Prado <sergio@garciparedes.me>
2021-02-25 18:28:40 +0100

    * Add base structure.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let input = read_input()?;
+    let solution = solve(input);
+    write_output(solution);
+    return Ok(());
+}
+
+fn read_input() -> io::Result<String> {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+
+
+    return Ok(buffer);
+}
+
+fn solve(input: String) -> String {
+    input
+}
+
+fn write_output(output: String) {
+    println!("{}", output);
 }
