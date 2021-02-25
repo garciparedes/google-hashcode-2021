29488ee5dc392c426dea5d1a9fbee259e3098601
Sergio García Prado <sergio@garciparedes.me>
2021-02-25 20:47:24 +0100

    * Fix stupid bug.

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@ set -e
 git archive --format zip --output solution.zip HEAD
 
 cargo run < res/a.txt > a.out
-cargo run < res/b.txt > a.out
+cargo run < res/b.txt > b.out
 cargo run < res/c.txt > c.out
 cargo run < res/d.txt > d.out
 cargo run < res/e.txt > e.out
